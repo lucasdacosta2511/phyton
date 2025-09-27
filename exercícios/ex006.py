b02eed035031a8ba609e800cf8766dc8611c8ea2
lucasdacosta2511 <lucascosta2511@gmail.com>
2025-09-27 19:44:49 -0300

    novos exercicios

---
+++ exercícios/ex006.py
@@ -0,0 +1,7 @@
+n = int(input('Digite um númaro'))
+d = n*2
+t = n*3
+r = pow(n, 0.5)
+print('O dobro de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}.'.format(n, t))
+print('A raíz quadrada de {} é igual a {:.2f}.'.format(n, r))
--- teste001.py
+++
@@ -1,6 +0,0 @@
-d = float(input('Distância'))
-t = float(input('tempo'))
-v = d/t
-
-if not d == None :
-    print(v)
