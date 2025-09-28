bddb03a7df8a2f1b9334016d2928c7434fc59305
lucasdacosta2511 <lucascosta2511@gmail.com>
2025-09-27 21:28:33 -0300

    novos exercícios

---
+++ exercícios/ex007.py
@@ -0,0 +1,4 @@
+p = float(input('Primeira nota do aluno'))
+s = float(input('Segunda nota do aluno'))
+m = (p+s)/2
+print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(p, s,m))
---
+++ exercícios/ex008.py
@@ -0,0 +1,8 @@
+medida = float(input('Digite uma distancia em metros: '))
+cm = medida * 100
+mm = medida * 1000
+dm = medida * 10
+dam = medida / 10
+hm = medida / 100
+km = medida / 1000
+print('A medida de {}m corresponde a \n{}km \n{}hm \n{}dam \n{:.0f}dm \n{:.0f}cm \n{:.0f}mm'.format(medida, km, hm, dam, dm, cm, mm))
