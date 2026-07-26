aab8f805b1676146a1c158569d4caf9d8cfc9db5
lucasdacosta2511 <lucascosta2511@gmail.com>
2026-07-26 09:59:16 -0300

    .

--- exercícios/ex045.py
+++ exercícios/ex045.py
@@ -8,22 +8,26 @@ print('''Suas opções
 [ 2 ] TESOURA'''
 )
 jogador = int(input("Qual é a sua jogada? "))
-print('JO')
-sleep(1)
-print('KEN')
-sleep(1)
-print('PO')
-print('O computador jogou {}'.format(itens[computador]))
-print('O jogador jogou {}'.format(itens[jogador]))
+
+if jogador<=3:
+    print('JO')
+    sleep(1)
+    print('KEN')
+    sleep(1)
+    print('PO')
+    print('O computador jogou {}'.format(itens[computador]))
+    print('O jogador jogou {}'.format(itens[jogador]))
+else:
+    print('JOGADA INVÁLIDA!!!')
 if computador == 0:
+    if jogador >= 3:
+        print('Jogada INVALIDA!')
     if jogador == 0:
         print('EMPATE')
     elif jogador == 1:
         print('JOGADOR VENCE')
     elif jogador == 2:
         print('COMPUTADOR VENCE')
-    else:
-        print('JOGADA INVÁLIDA')
 elif computador == 1:
     if jogador == 0:
         print('COMPUTADOR VENCE')
@@ -31,8 +35,6 @@ elif computador == 1:
         print('EMPATE')
     elif jogador == 2:
         print('JOGADOR VENCE')
-    else:
-        print('JOGADA INVÁLIDA')
 elif computador == 2:
     if jogador == 0:
         print('JOGADOR VENCE')
@@ -40,5 +42,3 @@ elif computador == 2:
         print('COMPUTADOR VENCE')
     elif jogador == 2:
         print('EMPATE')
-    else:
-        print('JOGADA INVÁLIDA')
